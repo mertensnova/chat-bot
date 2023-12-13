45cd0eb206fa6874130cedb187024262b6cc42bc
amr8644 <amr8644@gmail.com>
2023-12-13 18:30:15 +0300

    small AI bot

--- next.config.js
+++
@@ -1,9 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-   reactStrictMode: true,
-   images: {
-      domains: ["ih0.redbubble.net"],
-   },
-};
-
-module.exports = nextConfig;
